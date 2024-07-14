91b6ddfbe969a0deccc79daa91d09caf23cd61c7
julienbrg <julien@strat.cc>
2024-07-14 11:40:10 +0200

    Add tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import * as jsonfile from 'jsonfile';
+import * as path from 'path';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  const dbFilePath = path.resolve(__dirname, '..', 'src', 'users.json');
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,10 +18,22 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterEach(async () => {
+    await app.close();
+    const db = await jsonfile.readFile(dbFilePath);
+    db.users = db.users.filter((user) => user.id !== 'testuser');
+    await jsonfile.writeFile(dbFilePath, db);
+  });
+
+  it('/webauthn/register (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/webauthn/register')
+      .send({ userId: 'testuser' })
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toHaveProperty('rp');
+        expect(response.body).toHaveProperty('user');
+        expect(response.body).toHaveProperty('challenge');
+      });
   });
 });
