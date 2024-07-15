8c2e57f8aef844df22ff89e62bbf53a498c358f6
julienbrg <julien@strat.cc>
2024-07-15 13:05:56 +0200

    add corsOptions

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3001', // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
 
   const config = new DocumentBuilder()
     .setTitle('Myst API')
